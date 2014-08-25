25e0cca78844f5abc604355b34c6b28e1496056d
PabloVillagran <pdvillc@gmail.com>
2014-08-24 23:50:54 -0600

    Jugador1 e Interfaz.

--- LoveLetter 201313783/src/llGame/Interfaz.java
+++ LoveLetter 201313783/src/llGame/Interfaz.java
@@ -5,31 +5,42 @@ import java.util.Scanner;
 
 class Interfaz {
 	Scanner sc = new Scanner(System.in);
-	int op;
 	public int tokens=7;
+	public static String reglas []={"--(Valor) Carta <# de cartas en el maso>: descripcion--"
+			, "(1) Guard <5>: trata de adivinar la carta del oponente, si es correcto el oponente pierde la ronda!"
+			, "(2) Priest <2>: ve la carta del oponente!"
+			, "(3) Baron <2>: compara tu carta y la del oponente, la carta mas alta gana la ronda!"
+			, "(4) HandMaid <2>: Protege al usuario de los efectos del oponente por 1 turno!"
+			, "(5) Prince <2>: El jugador descarta su mano o la del oponente, sin aplicar los efectos!"
+			, "(6) King <1>: Intercambia las manos de los jugadores!"
+			, "(7) Countess <1>: Carta mas alta, se debe descartar si hay un rey o principe en la mano!"
+			, "(8) Princess <1>: Si se descarta a la princesa se pierde la ronda!"};
 	
 	public Interfaz(){
 		this.menu1();
 	}
 	
 	void menu1(){
+		String op;
 		System.out.println("-=Bienvenido a Love Letter=-\n"
 				+ "(1) Jugar partida \n"
 				+ "(2) Seleccionar el numero de Tokens a Jugar\n"
 				+ "(3) Mostrar las reglas del juego\n"
 				+ "(4) Salir");
 		System.out.print("¿Que desea hacer? ");
-		op = sc.nextInt();
+	
+		op = sc.next();
+		
 		switch(op){
-		case 1:
+		case "1":
 			/*try{
 				Runtime.getRuntime().exec("cls");
 			}catch(IOException e){
 				e.printStackTrace();
 			}*/
-			System.out.println("Jugar partida.");
+			Juego juego = new Juego(tokens);
 			break;
-		case 2:
+		case "2":
 			System.out.println("Seleccione el numero de tokes a jugar:\n "
 					+ "(numero impar entre 1 y 7)");
 			tokens = sc.nextInt();
@@ -40,10 +51,10 @@ class Interfaz {
 			}
 			this.menu1();
 			break;
-		case 3:
+		case "3":
 			this.instrucciones();
 			break;
-		case 4:
+		case "4":
 			System.exit(0);
 			break;
 		default:
@@ -63,16 +74,12 @@ class Interfaz {
 		String de = sc.next();
 		System.out.println(de);
 		if(de.contains("s")){
-			System.out.println("--(Valor) Carta <# de cartas en el maso>: descripcion--\n"
-					+ "(1) Guard <4>: trata de adivinar la carta del oponente, si es correcto el oponente pierde la ronda\n"
-					+ "(2) Priest <2>: ve la carta del oponente\n"
-					+ "(3) Baron <2>: compara tu carta y la del oponente, la carta mas alta gana la ronda.\n"
-					+ "(4) HandMaid <2>: Protege al usuario de los efectos del oponente por 1 turno.\n"
-					+ "(5) Prince <2>: El jugador descarta su mano o la del oponente, sin aplicar los efectos.\n"
-					+ "(6) King <1>: Intercambia las manos de los jugadores\n"
-					+ "(7) Countess <1>: Carta mas alta, se debe descartar si hay un rey o principe en la mano\n"
-					+ "(8) Princess <1>: Si se descarta a la princesa se pierde la ronda.");
-		this.menu1();
+			for(int i=0;i<=8;i++){
+				System.out.println(reglas[i]);
+			}
+			System.out.println("Continuar? ");
+			System.out.println(sc.next());
+			this.menu1();
 		}else{
 			this.menu1();
 		}
